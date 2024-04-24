14fc07a9056e08dd2a9a1ba84477ff9b49d09d85
M39an90 <105782379+M39an90@users.noreply.github.com>
2024-04-24 14:18:24 +0000

    first commit

---
+++ practice.py
@@ -0,0 +1,9 @@
+import random
+
+name = input ("Enter your name: ")
+print("Hello " + name + "!")
+
+character = random.choice(["Sherlock", "Watson"])
+
+print(name + " you have been assigned as " + character + ".")
+
